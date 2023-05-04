4ad5bb4bdbfcf64ae2e1e73df1a20f49994dc339
Stephen <stephen_1996@live.ca>
2023-05-04 14:18:42 -0700

    creating game panel class

---
+++ src/GamePanel.java
@@ -0,0 +1,7 @@
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16 x 16 tile, default size of 2D character
+
+}

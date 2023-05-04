88fe61a31e7958d63878041a946f90171e790b9e
Stephen <stephen_1996@live.ca>
2023-05-04 15:15:04 -0700

    set up WASD keys for movement

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -2,6 +2,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -10,7 +11,21 @@ public class KeyHandler implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        //returns number of key that was pressed
+        int code = e.getKeyCode();
+        //user pressing key
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
     }
 
     @Override

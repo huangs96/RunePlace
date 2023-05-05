613cb09c74bf07c1fa11bde9dd41acbb222c3679
Stephen <stephen_1996@live.ca>
2023-05-05 15:26:13 -0700

    adding character animation through sprite counter

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -7,7 +7,6 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Objects;
 
 
 public class Player extends Entity {
@@ -65,6 +64,9 @@ public class Player extends Entity {
                direction = "right";
                x += speed;
           }
+          //counter for char animation
+          spriteCounter++;
+
      }
 
      public void draw (Graphics2D g2) {
@@ -72,16 +74,48 @@ public class Player extends Entity {
 
           switch(direction) {
                case "up":
-                    image = up1;
+                    if(spriteNum == 1) {
+                         image = up1;
+                    }
+                    if(spriteNum == 2) {
+                         image = up2;
+                    }
+                    if(spriteNum == 3) {
+                         image = up3;
+                    }
                     break;
                case "down":
-                    image = down1;
+                    if(spriteNum == 1) {
+                         image = down1;
+                    }
+                    if(spriteNum == 2) {
+                         image = down2;
+                    }
+                    if(spriteNum == 3) {
+                         image = down3;
+                    }
                     break;
                case "right":
-                    image = right1;
+                    if(spriteNum == 1) {
+                         image = right1;
+                    }
+                    if(spriteNum == 2) {
+                         image = right2;
+                    }
+                    if(spriteNum == 3) {
+                         image = right3;
+                    }
                     break;
                case "left":
-                    image = left1;
+                    if(spriteNum == 1) {
+                         image = left1;
+                    }
+                    if(spriteNum == 2) {
+                         image = left2;
+                    }
+                    if(spriteNum == 3) {
+                         image = left3;
+                    }
                     break;
           }
 
